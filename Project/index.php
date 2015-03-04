3d5c143109f49cd33c3e4c12790a8427b1c6f174
sirmofan <per.wj.ahlbom@gmail.com>
2015-03-04 22:47:43 +0100

    added new files
    added some functionalety to index

---
+++ Project/index.php
@@ -0,0 +1,25 @@
+<html>
+	<header> <title>Production </title></header>
+	<body>
+		<h1>Production</h1>
+		<p>Click on of the buttons to preforma a task</p>
+		<h2>Finde a pallet</h2>
+		<p>To finde a pallet click the button Finde a pallet</p>
+		<form method="get" action="searchpallet1.php">
+			<input type=submit value="Finde a pallet">
+		</form>
+		<h2>Block pallet</h2>
+		<p>To block a pallet click the button Block a pallet </p>
+		<form method="get" action="blockpallet1.php">
+			<input type=submit value="Block a pallet">
+		</form>
+
+		</form>
+		<h2>Creat a pallet</h2>
+		<p>To creat a pallet click on the button Creat a pallet</p>
+		<form method="get" action="creatpallet1.php">
+			<input type=submit value="Creat a pallet">
+		</form>
+	</body>
+
+</html>
