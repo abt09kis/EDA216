3ef6ab0b574a303374ce557819f07175b317f6b7
Kevin Ispiroglu Lindblad <abt09kis@student.lu.se>
2015-03-04 19:05:07 +0100

    Created tables and added some data

--- Project/sql_creat_table.sql
+++ Project/sql_creat_table.sql
@@ -24,7 +24,7 @@ create table Cookies (
 cookieName varchar(30)  not null,
 primary key(cookieName)
 );
-create table Recipe (
+create table Recipes (
 cookieName varchar(30) not null,
 ingredientName varchar(30) not null,
 quantity integer not null,
@@ -44,7 +44,9 @@ create table Orders  (
 orderNbr integer auto_increment,
 nbrPallet integer not null,
 orderDate date not null,
-primary key(orderNbr)
+customerName varchar(30) not null,
+primary key(orderNbr),
+foreign key(customerName) references Customers(customerName)
 );
 create table PalletsInOrders  (
 orderNbr integer not null,
@@ -63,6 +65,91 @@ customerId integer not null,
 orderNbr integer not null,
 primary key(orderNbr)
 );
+--Insert Data into the tables.
+--Ingredients
+insert into Ingredients values('Flour','1000000','2015-03-04','0');
+insert into Ingredients values('Butter','1000000','2015-03-04','0');
+insert into Ingredients values('Icing Sugar','1000000','2015-03-04','0');
+insert into Ingredients values('Roasted, chopped nuts','1000000','2015-03-04','0');
+insert into Ingredients values('Fine-ground nuts','1000000','2015-03-04','0');
+insert into Ingredients values('Ground, roasted nuts','1000000','2015-03-04','0');
+insert into Ingredients values('Bread crumbs','1000000','2015-03-04','0');
+insert into Ingredients values('Sugar','1000000','2015-03-04','0');
+insert into Ingredients values('Egg whites','1000000','2015-03-04','0');
+insert into Ingredients values('Chocolate','1000000','2015-03-04','0');
+insert into Ingredients values('Marzipan','1000000','2015-03-04','0');
+insert into Ingredients values('Eggs','1000000','2015-03-04','0');
+insert into Ingredients values('Potato starch','1000000','2015-03-04','0');
+insert into Ingredients values('Wheat flour','1000000','2015-03-04','0');
+insert into Ingredients values('Sodium bicarbonate','1000000','2015-03-04','0');
+insert into Ingredients values('Vanilla','1000000','2015-03-04','0');
+insert into Ingredients values('Chopped almonds','1000000','2015-03-04','0');
+insert into Ingredients values('Cinnamon','1000000','2015-03-04','0');
+insert into Ingredients values('Vanilla Sugar','1000000','2015-03-04','0');
+--Cookies
+insert into Cookies values('Nut ring');
+insert into Cookies values('Nut cookie');
+insert into Cookies values('Amneris');
+insert into Cookies values('Tango');
+insert into Cookies values('Almond delight');
+insert into Cookies values('Berliner');
+--Recipes
+--Nut ring
+insert into Recipes values('Nut ring', 'Flour','450');
+insert into Recipes values('Nut ring', 'Butter','450');
+insert into Recipes values('Nut ring', 'Icing sugar','190');
+insert into Recipes values('Nut ring', 'Roasted, chopped nuts', '225');
+--Nut Cookie
+insert into Recipes values('Nut cookie','Fine-ground nuts','750');
+insert into Recipes values('Nut cookie','Ground, roasted nuts','625');
+insert into Recipes values('Nut cookie','Bread crumbs','125');
+insert into Recipes values('Nut cookie','Sugar','375');
+insert into Recipes values('Nut cookie','Egg whites','350');
+insert into Recipes values('Nut cookie','Chocolate','50');
+--Amneris
+insert into Recipes values('Amneris','Marzipan','750');
+insert into Recipes values('Amneris','Butter','250');
+insert into Recipes values('Amneris','Eggs','250');
+insert into Recipes values('Amneris','Potato starch','25');
+insert into Recipes values('Amneris','Wheat flour','25');
+--Tango
+insert into Recipes values('Tango','Butter','200');
+insert into Recipes values('Tango','Sugar','250');
+insert into Recipes values('Tango','Flour','300');
+insert into Recipes values('Tango','Sodium bicarbonate','4');
+insert into Recipes values('Tango','Vanilla','2');
+--Almond Delight
+insert into Recipes values('Almonds delight','Butter','400');
+insert into Recipes values('Almonds delight','Sugar','270');
+insert into Recipes values('Almonds delight','Chopped almonds','279');
+insert into Recipes values('Almonds delight','Flour','400');
+insert into Recipes values('Almonds delight','Cinnamon','10');
+--Berliner
+insert into Recipes values('Berliner','Flour','350');
+insert into Recipes values('Berliner','Butter','250');
+insert into Recipes values('Berliner','Icing sugar','100');
+insert into Recipes values('Berliner','Eggs','50');
+insert into Recipes values('Berliner','Vanilla Sugar','5');
+insert into Recipes values('Berliner','Chocolate','50');
+--Customers
+insert into Customers values(customerId,'Helsingborg','Finkakor AB');
+insert into Customers values(customerId,'Malmö','Småbröd AB');
+insert into Customers values(customerId,'Landskrona','Kaffebröd AB');
+insert into Customers values(customerId,'Ystad','Bjudkakor AB');
+insert into Customers values(customerId,'Trelleborg','Kalaskakor AB');
+insert into Customers values(customerId,'Kristianstad','Partykakor AB');
+insert into Customers values(customerId,'Hässleholm','Gästkakor AB');
+insert into Customers values(customerId,'Perstorp','Skånekakor AB');
 
+--Orders
+insert into Pallets values(palletNbr,'Tango','2015-03-04','FALSE','TRUE');
+insert into Orders values(orderNbr,'2','2015-03-04','Finkakor AB');
 
+insert into Pallets values(palletNbr,'Berliner','2015-03-04','FALSE','TRUE');
+insert into Orders values(orderNbr,'5','2015-03-04','Gästkakor AB');
 
+insert into Pallets values(palletNbr,'Almond delight','2015-03-04','FALSE','TRUE');
+insert into Orders values(orderNbr,'10','2015-03-04','Kaffebröd AB');
+
+insert into Pallets values(palletNbr,'Nut cookie','2015-03-04','FALSE','TRUE');
+insert into Orders values(orderNbr,'8','2015-03-04','Bjudkakor AB');
